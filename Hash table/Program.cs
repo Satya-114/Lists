807cab2d6bc93fca89154ffdf3d96d330d7cf0d8
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 20:16:22 +0530

    Add files via upload

---
+++ Hash table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("001", ".Net");
+            ht.Add("002", "C#");
+            ht.Add("003", "ASP.Net");
+
+            ICollection keys = ht.Keys;
+
+            foreach (String k in keys)
+            {
+                Console.WriteLine(ht[k]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
